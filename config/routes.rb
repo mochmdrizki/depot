6407927c6577f017cf37bae9776b30402d6c9efd
mochmdrizki <rizki@41studio.com>
2017-06-02 14:20:14 +0700

    Atom feeds

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # get 'store/index'
   root 'store#index', as: 'store_index'
 
+  resources :products do
+    get :who_bought, on: :member
+  end
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
